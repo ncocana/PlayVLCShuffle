3d1b4053784a1c9d0baa9644709b7657849199df
ncocana <ncocana@cifpfbmoll.eu>
2022-12-21 19:28:43 +0100

    feat(playShuffleVLC_DAO): Check module dependencies and imports

--- src/playShuffleVLC_DAO.py
+++ src/playShuffleVLC_DAO.py
@@ -38,6 +38,35 @@ def checkPlaySuffle(playList):
 ## RUTINAS DE UTILIDADES ##
 
 
+def check_modules():
+
+    try:
+        import sys
+    except(ImportError):
+        raise SystemExit #exit()
+
+    dependencies = ['shlex', 'os', 'subprocess', 'random', 
+                    # 'touche turtle' # disparar bloque try
+                    ]
+
+    try:
+        for dependency in dependencies:
+            if dependency not in (sys.stdlib_module_names):
+                raise ModuleNotFoundError(str(dependency)) # Builtin exception
+    except ModuleNotFoundError as e:
+        # sys,exit() cleanup actions specified by finally clauses of try statements are honored
+        e_type, e_name, e_trace = sys.exc_info()
+        sys.exit("Modulo %s no encontrado" % (str(e_name)))
+    except AttributeError as e:
+        # python exception message capturing
+        sys.exit(str(e))
+    else:
+        # scope de variables locales en el try /except
+        print("Dependencias %s presentes en el sistema" % (', '.join(dependencies)))
+    finally:
+        print("Dependencias de modulos chequeadas")
+
+
 def seleccionaCancionRandom(libreria):
 
     assert isinstance(libreria, dict)
@@ -159,6 +188,8 @@ def playShuffle(libreria, playList):
 
 
 def playShuffleVLC(libreria, playList):
+
+    check_modules()
 
     playShuffle(libreria, playList)
 
